690ce3e21696a9601b6d6b061042b6ebcbca70e5
sravya <sravyasree052k@gmail.com>
2023-01-16 14:34:25 +0530

    1st commit code

---
+++ list.py
@@ -0,0 +1,81 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "squares of num\n",
+      "4\n",
+      "16\n",
+      "36\n",
+      "64\n",
+      "100\n",
+      "cubes of num\n",
+      "8\n",
+      "64\n",
+      "216\n",
+      "512\n",
+      "1000\n"
+     ]
+    }
+   ],
+   "source": [
+    "num =[2,4,6,8,10]\n",
+    "print(\"squares of num\")\n",
+    "for i in num:\n",
+    "    print(i**2)\n",
+    "    \n",
+    "print(\"cubes of num\")    \n",
+    "for i in num:\n",
+    "    print(i**3)   "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
